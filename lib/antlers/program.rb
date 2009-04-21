f83419dcb8b3bd9c6b4c7100fe898029692e297e
G. Brandon Hoyt <qrprat77@gmail.com>
2009-04-21 14:52:54 -0400

    Doing some learning about rspec

--- lib/antlers/program.rb
+++ lib/antlers/program.rb
@@ -1,4 +1,11 @@
 module Antlers
 	class Program
+		def initialize(messenger)
+			@messenger = messenger
+		end
+		def start
+  			# @messenger.puts("Please make one of the following menu selections ") #example as written from the RSPEC book, doesn't pass.
+			puts "Please make one of the following menu selections " # This example passes
+		end
 	end
 end
--- spec/antlers/program_spec.rb
+++ spec/antlers/program_spec.rb
@@ -4,7 +4,9 @@ module Antlers
 	describe Program do
 		context "Starting up" do 
 			it "should display an options menu" do
-				program.should_receive(:puts).with("Please make a selection: ")
+				messenger = mock("messenger")
+				program = Program.new(messenger)
+				program.should_receive(:puts).with("Please make one of the following menu selections ")
 				program.start
 			end
 		end
