cdd676d78c2fd4a95f80dd2bd125291d2e2e24dd
G. Brandon Hoyt <qrprat77@gmail.com>
2009-04-21 10:22:19 -0400

    making the RSpec book work for me...

--- features/step_definitions/antlers.rb
+++ features/step_definitions/antlers.rb
@@ -2,10 +2,11 @@ Given /^I am not yet designing an antenna$/ do
 end
 
 When /^I start the program$/ do
-  program = Antlers::Menu.new
-  @message = program.show
+  @messenger = StringIO.new
+   program = Antlers::Program.new(@messenger)
+   program.start
 end
 
 Then /^antlers should prompt "(.*)"$/ do |message|
-	@message.should include(message)
+	@messager.string.split("\n").should include(message)
 end
--- spec/antlers/program_spec.rb
+++ spec/antlers/program_spec.rb
@@ -2,5 +2,9 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module Antlers
 	describe Program do
+		context "Starting up" do 
+			it "should display an options menu" do
+				program.should_receive(:puts).with ("Please make a selection: ")
+				program.start
 	end
 end
