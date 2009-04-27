f4f415689ff1ae313af62c5696453f311744ca15
G. Brandon Hoyt <qrprat77@gmail.com>
2009-04-27 01:30:28 -0400

    refactoring specs into cleanliness

--- spec/antlers/program_spec.rb
+++ spec/antlers/program_spec.rb
@@ -3,18 +3,19 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 module Antlers
 	describe Program do
 		context "Starting up" do 
+			before(:each) do
+				@messenger = mock("messenger").as_null_object
+				@program = Program.new(@messenger)
+			end
+			
 			it "should give a welcome message" do
-				messenger = mock("messenger").as_null_object
-				program = Program.new(messenger)
-				messenger.should_receive(:puts).with('This is ANTLERS! A  program for designing antennas for the ham radio operator written in ruby')
-				program.start
+				@messenger.should_receive(:puts).with('This is ANTLERS! A  program for designing antennas for the ham radio operator written in ruby')
+				@program.start
 			end
 			
 			it "should display an options menu" do
-				messenger = mock("messenger").as_null_object
-				program = Program.new(messenger)
-				messenger.should_receive(:puts).with("Please make one of the following menu selections ")
-				program.start
+				@messenger.should_receive(:puts).with("Please make one of the following menu selections ")
+				@program.start
 			end
 		end
 	end
