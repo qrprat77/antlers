dba2326edbe1ebd32b8f2a891bddaafbb9795010
GB Hoyt <qrprat77@gmail.com>
2011-08-28 07:04:59 -0400

    New branch and menu modification

--- lib/antlers/program.rb
+++ lib/antlers/program.rb
@@ -6,6 +6,12 @@ module Antlers
 		def start
 			@messenger.puts 'This is ANTLERS! A  program for designing antennas for the ham radio operator written in ruby'
   			@messenger.puts 'Please make one of the following menu selections '
+  			show_menu(:main)
+		end
+		
+		def show_menu(menu)
+			@current_menu = Antlers::Menu.new(menu)
+			@messenger.puts @current_menu.title
 		end
 	end
 end
