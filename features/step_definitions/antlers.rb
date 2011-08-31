71ed432afe00af377b9bfc80830a9662aeab6884
GB Hoyt <qrprat77@gmail.com>
2011-08-31 09:02:58 -0400

    recovered step defs

--- features/step_definitions/antlers.rb
+++ features/step_definitions/antlers.rb
@@ -3,3 +3,32 @@ def newantlers
   @program = Antlers::Program.new(@messenger)
   @program.start
 end
+
+Given /^I am not yet designing an antenna$/ do
+end
+
+When /^I start the program$/ do
+  newantlers
+end
+
+Then /^antlers should prompt "(.*)"$/ do |message|
+	@messenger.string.split("\n").should include(message)
+end
+
+Given /^the program has started$/ do
+ 	newantlers
+end
+
+Given /^the menu is (.*)$/ do |menu|
+  newantlers
+  @messenger.string.split("\n").should include(menu) # express the regexp above with the code you wish you had
+end
+
+When /^I choose A$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should launch antlers$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
